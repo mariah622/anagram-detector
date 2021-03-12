bb8d9b80c9b21417ca676aa1807c0b5724290d26
Mariah Morales <mjustine622@gmail.com>
2021-03-12 14:24:14 -0500

    commit tree bdeee8ece4f2eeb0e317c91d335308231105c6fe

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram 
+    attr_accessor :word
+
+    def initialize(word)
+      @word = word 
+    end
+
+    def match(arrays)
+        split_word = @word.split("").sort
+        arrays.select {|x| x.split("").sort == split_word}
+      end
+  end
